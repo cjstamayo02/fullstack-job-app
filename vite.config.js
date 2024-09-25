baf5d0d8a4e41c5f00af23b2abcda3765deb1a25
Christian Tamayo <20191242@cityofmalabonuniversity.edu.ph>
2024-09-25 14:29:45 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
